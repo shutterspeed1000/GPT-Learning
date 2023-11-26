13eb70ab411c17ab5868f51369587862b25de657
Sean Worrell <spworrell@gmail.com>
2023-11-25 20:21:02 -0500

    Add new dependencies and update existing ones

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// router paths for site
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  MDBContainer,
+  MDBInput,
+  MDBCheckbox,
+  MDBBtn,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+
+function App() {
+  return (
+    <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <MDBInput
+        wrapperClass="mb-4"
+        label="Email address"
+        id="form1"
+        type="email"
+      />
+      <MDBInput
+        wrapperClass="mb-4"
+        label="Password"
+        id="form2"
+        type="password"
+      />
+
+      <div className="d-flex justify-content-between mx-3 mb-4">
+        <MDBCheckbox
+          name="flexCheck"
+          value=""
+          id="flexCheckDefault"
+          label="Remember me"
+        />
+        <a href="!#">Forgot password?</a>
+      </div>
+
+      <MDBBtn className="mb-4">Sign in</MDBBtn>
+
+      <div className="text-center">
+        <p>
+          Not a member? <a href="#!">Register</a>
+        </p>
+        <p>or sign up with:</p>
+      </div>
+    </MDBContainer>
+  );
+}
+
+export default App;
