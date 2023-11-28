66aec457c98a96bb52fd9d62cb08dd42ad407ccf
Sean Worrell <spworrell@gmail.com>
2023-11-27 23:14:52 -0500

    port fixed

--- server/index.js
+++ server/index.js
@@ -21,6 +21,6 @@ app.get("*", (req, res) => {
 });
 
 // start express
-app.listen(3000, () => {
-  console.log("App listening on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("App listening on port process.env.PORT");
 });
